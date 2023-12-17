94ef588177b6f90b08a49f2f616267231b6cefd8
Dipak-jagadale <dipakjagadale999@gmail.com>
2023-12-17 21:23:27 +0530

    sql squeery

---
+++ emp_sql_queery.sql
@@ -0,0 +1,12 @@
+show databases;
+ use sys;
+show table;
+select * from metrics;
+describe metrics;
+create database sql_intro;
+show databases;
+
+create table emp_details (Name	varchar(25), Age int, Sex char(1),
+doj date, city varchar(15),salary float);
+
+describe emp_details;
